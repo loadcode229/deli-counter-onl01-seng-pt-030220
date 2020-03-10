8ac40f661947f65dd09f22a331c23faeedf0051d
Adam Priest <sepone229@gmail.com>
2020-03-10 10:10:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,7 @@ def take_a_number(line, new_person)
   line << new_person
   puts "Welcome, #{new_person}. You are number #{line.size} in line."
 end
+
+def now_serving(line, take_a_number)
+  puts "Currently serving #{line}."
+end
