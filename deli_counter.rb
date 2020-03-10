3caef8381a0e3babdb2fff2277b0ea04e7f87ed0
Adam Priest <sepone229@gmail.com>
2020-03-10 10:13:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,5 +19,7 @@ end
 
 def now_serving(line, take_a_number)
   if take_a_number > 0
-    puts "The line is currently"
+    puts "The line is currentl"
+  else
+    puts "The line is currently empty."
 end
