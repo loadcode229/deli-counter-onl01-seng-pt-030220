f9f50ee87a77a417056f02774030a6e1711805f5
Adam Priest <sepone229@gmail.com>
2020-03-10 10:14:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 
 def now_serving(line, take_a_number)
   if line > 0
-    puts "Currently serving #{take_a_number}."
+    puts "Currently serving #{line}."
   else
     puts "The line is currently empty."
   end
