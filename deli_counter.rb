03670d796f1c1c5a5361955e0032f34874613717
Adam Priest <sepone229@gmail.com>
2020-03-10 10:22:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,8 @@ def now_serving(line)
   if line.size == 0
     puts "There is nobody waiting to be served!"
   else
-    puts ""
+    puts "Currently serving #{line[0]}"
+    line.shift
   end
 end
 
